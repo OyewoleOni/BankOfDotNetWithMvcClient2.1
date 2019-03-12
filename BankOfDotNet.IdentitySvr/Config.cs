abd96150f4b5bdb0dfb481b01c66ca2e4f1550b2
oni oyewole <ooni@eminenttechnology.com>
2019-03-12 16:47:10 +0100

    Add project files.

---
+++ BankOfDotNet.IdentitySvr/Config.cs
@@ -0,0 +1,95 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankOfDotNet.IdentitySvr
+{
+    public class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+        }
+
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "Oyewole",
+                    Password = "password"
+                },
+                 new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "Oni",
+                    Password = "password"
+                }
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetAllApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("bankOfDotNetApi", "Customer Api for BankOfDotNet")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "bankOfDotNetApi" }
+                },
+
+                new Client
+                {
+                    ClientId ="ro.client",
+
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+
+                     ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "bankOfDotNetApi" }
+                }, 
+
+                new Client
+                {
+                    ClientId = "mvc",
+                    ClientName = "MVC Client",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+
+                   RedirectUris = {"http://localhost:5004/signin-oidc"},
+                    PostLogoutRedirectUris = {"http://localhost:5004/signout-callback-oidc"},
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    }
+                }
+            };
+        }
+    }
+}
