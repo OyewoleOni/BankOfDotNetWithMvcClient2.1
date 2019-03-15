b0123e41c97a6d91cdf5fbc3503b94e540d200de
oni oyewole <ooni@eminenttechnology.com>
2019-03-15 19:16:45 +0100

    Identity Server4 Entity Framework

--- BankOfDotNet.IdentitySvr/Startup.cs
+++ BankOfDotNet.IdentitySvr/Startup.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BankOfDotNet.IdentitySvr
@@ -17,17 +23,43 @@ namespace BankOfDotNet.IdentitySvr
         {
             services.AddMvc();
 
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("Appsettings.json", false)
+                .Build();
+
+            string connectionString = config.GetSection("connectionString").Value;
+
+            var migrationAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+
             services.AddIdentityServer()
                 .AddDeveloperSigningCredential()
-                .AddInMemoryIdentityResources(Config.GetIdentityResources())
-                .AddInMemoryApiResources(Config.GetAllApiResources())
-                .AddInMemoryClients(Config.GetClients())
-                .AddTestUsers(Config.GetUsers());
+                //.AddInMemoryIdentityResources(Config.GetIdentityResources())
+                //.AddInMemoryApiResources(Config.GetAllApiResources())
+                //.AddInMemoryClients(Config.GetClients())
+                .AddTestUsers(Config.GetUsers())
+                // Configuration Store: clients and resources
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = b =>
+                        b.UseSqlServer(connectionString,
+                        sql => sql.MigrationsAssembly(migrationAssembly));
+                })
+                //Operational Store: token, consents, codes
+                .AddOperationalStore(option =>
+                {
+                    option.ConfigureDbContext = b =>
+                        b.UseSqlServer(connectionString,
+                        sql => sql.MigrationsAssembly(migrationAssembly));
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            InitializeIdentityServerDataBase(app);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -37,6 +69,46 @@ namespace BankOfDotNet.IdentitySvr
 
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
+        }
+
+        private void InitializeIdentityServerDataBase(IApplicationBuilder app)
+        {
+            using ( var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+
+                //Seed the Data
+                //Seed the Clients
+                if ( !context.Clients.Any() )
+                {
+                    foreach ( var client in Config.GetClients())
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+                //Seed the IdentityResources
+                if ( !context.IdentityResources.Any() )
+                {
+                    foreach ( var resource in Config.GetIdentityResources())
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+                //Seed the Clients
+                if ( !context.ApiResources.Any() )
+                {
+                    foreach ( var apiResource in Config.GetAllApiResources() )
+                    {
+                        context.ApiResources.Add(apiResource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
